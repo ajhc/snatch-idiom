7d4e318b3b917441e25cba09c48487dff7d32f8b
Kiwamu Okabe <kiwamu@debian.or.jp>
2013-12-13 01:43:24 +0900

    Run make install with sudo

--- travis-ci/setup.sh
+++ travis-ci/setup.sh
@@ -2,4 +2,4 @@
 sudo apt-get -qq update
 sudo apt-get -qq -y install python
 git clone https://github.com/yuzutechnology/stdiotest.git
-cd stdiotest && make install
+cd stdiotest && sudo make install
